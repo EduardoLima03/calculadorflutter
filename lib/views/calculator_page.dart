99feeaec2bbdea75d94e98053ae3a9ab465c9ccf
Carlos Lima <tec.carlos.lima@gmail.com>
2020-12-28 17:03:25 -0300

    layoutBuildder criado

--- lib/views/calculator_page.dart
+++ lib/views/calculator_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class CalculatorPage extends StatefulWidget {
@@ -9,8 +8,21 @@ class CalculatorPage extends StatefulWidget {
 class _CalculatorPageState extends State<CalculatorPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).primaryColor,
+    return Scaffold(
+      body: LayoutBuilder(builder: (_, constraints) {
+        return Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).padding.top,
+            ),
+            Container(
+              color: Colors.red,
+              width: constraints.maxWidth,
+              height: 10,
+            )
+          ],
+        );
+      }),
     );
   }
 }
