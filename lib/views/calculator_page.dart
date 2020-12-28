bf03f39166a19f6a2e07cde968d60094180a23a6
Carlos Lima <tec.carlos.lima@gmail.com>
2020-12-28 15:11:20 -0300

    estrutura inicial

---
+++ lib/views/calculator_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class CalculatorPage extends StatefulWidget {
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+    );
+  }
+}
